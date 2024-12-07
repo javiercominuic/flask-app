8db78198729d85e043946c7c907524d2b0c1663a
javiercomin <javier.comin@hotmail.com>
2024-12-07 11:44:29 +0100

    Primer commit: Añadir README con descripción del proyecto

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '¡Bienvenidos a mi aplicación Flask!'
+
+@app.route('/about')
+def about():
+    return 'Acerca de esta página'
+
+@app.route('/contact')
+def contact():
+    return 'Contáctanos en flask@example.com'
+
+@app.route('/hobbies')
+def hobbies():
+    return 'Mis hobbies son programar y leer'
+
+if __name__ == '__main__':
+    app.run(debug=True)
